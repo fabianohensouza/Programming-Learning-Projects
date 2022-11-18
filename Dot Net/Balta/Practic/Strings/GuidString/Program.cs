567fd3f70b86316dc00c7497fff0b4604b30976f
Fabiano Souza <fabianohensouza@gmail.com>
2022-11-18 20:17:35 -0300

    Stutying dotnet

---
+++ Dot Net/Balta/Practic/Strings/GuidString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GuidString // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+
+            id = new Guid("432b9b0f-6066-464c-9cd8-d99cf59e9797");
+            Console.WriteLine(id);
+        }
+    }
+}
---
+++ Dot Net/Balta/Practic/Strings/Interpolacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interpolacao // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var price = 10.2;
+            var product = "Television";
+            //var text = "The product price is $" + price;
+            //var text = string.Format("The {1} price is ${0} only in the hot sale!", price, product);
+            //var text = $"The {product} price is ${price} only in the hot sale!";
+            //var text = $@"The {product} price is ${price} 
+            //only in the hot sale!";
+            var text = $"The {product} price is ${price} \nonly in the hot sale!";
+
+            Console.WriteLine(text);
+        }
+    }
+}
