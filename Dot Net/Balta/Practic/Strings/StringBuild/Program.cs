94d6bc81dce60bc4bfd152e813a9a7c07ef29ae0
Fabiano Souza <fabianohensouza@gmail.com>
2022-11-19 11:12:59 -0300

    Stutying dotnet

---
+++ Dot Net/Balta/Practic/Strings/StringBuild/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuild // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new StringBuilder();
+            text.Append("This Text Is a Test ");
+            text.Append("of the method Append ");
+            text.Append("\n");
+            text.Append("provided by .NET");
+
+            Console.WriteLine(text.ToString());
+        }
+    }
+}
