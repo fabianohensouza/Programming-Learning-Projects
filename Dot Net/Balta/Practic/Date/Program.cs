f28b9b0143ab34cf33f55ef8b9a4554434657f34
Fabiano Souza <fabianohensouza@gmail.com>
2022-11-22 20:18:01 -0300

    Studying .NET

---
+++ Dot Net/Balta/Practic/Date/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Date // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            //var data = new DateTime();
+
+            //var data = new DateTime(2020, 10, 12, 8, 23, 14);
+            var data = DateTime.Now;
+            var formatada = String.Format("{0:d}/{0:MM}/{0:yyyy}.", data);
+            var formatada2 = String.Format("{0:R}.", data);
+
+            //Console.WriteLine(formatada);
+            //Console.WriteLine(formatada2);
+            //Console.WriteLine(data.Year);
+            //Console.WriteLine(data.DayOfWeek);
+            //Console.WriteLine((int)data.DayOfWeek);
+            Console.WriteLine(data.AddDays(900));
+            Console.WriteLine(data.AddMonths(900));
+            Console.WriteLine(data.AddYears(900));
+        }
+    }
+}
