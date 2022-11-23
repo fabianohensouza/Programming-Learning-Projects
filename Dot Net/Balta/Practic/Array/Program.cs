78764f5ff2a7abe51ce0438ee0b3c2cb97f3784d
Fabiano Souza <fabianohensouza@gmail.com>
2022-11-23 20:00:38 -0300

    Stuying code

---
+++ Dot Net/Balta/Practic/Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Array // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            //int[] meuArray = new int[5];
+            //var meuArray = new int[5];
+            var meuArray = new int[5] { 89, 74, 43, 14, 22 };
+
+            var pessoa = new Pessoa() { Id = 89, Name = "Jose" };
+
+            Console.WriteLine(meuArray[2]);
+
+            for (int index = 0; index < meuArray.Length; index++)
+                Console.WriteLine(meuArray[index]);
+
+            Console.WriteLine("--------------------------------");
+
+            foreach (var item in meuArray)
+                Console.WriteLine(item);
+
+            Console.WriteLine("--------------------------------");
+        }
+
+        public struct Pessoa
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ Dot Net/Balta/Practic/Moeda/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moeda // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal value = 15029.69m;
+            decimal value2 = 15029.45m;
+
+            Console.WriteLine(value);
+
+            Console.WriteLine(
+                value.ToString(
+                    "C",
+                    CultureInfo.CreateSpecificCulture("pt-BR")
+                )
+            );
+
+            Console.WriteLine(Math.Round(value));
+            Console.WriteLine(Math.Round(value2));
+
+            Console.WriteLine(Math.Ceiling(value));
+            Console.WriteLine(Math.Ceiling(value2));
+
+            Console.WriteLine(Math.Floor(value));
+            Console.WriteLine(Math.Floor(value2));
+        }
+    }
+}
