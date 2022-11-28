1f211ec8db55ad7fe25622dfa1a5653818d9bace
Fabiano Souza <fabianohensouza@gmail.com>
2022-11-28 20:09:35 -0300

    Studying code

---
+++ Dot Net/Balta/Fundamentos da Orientação a Objetos/OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Payment
+    {
+        DateTime Vencimento;
+
+        public int MyProperty { get; set; }
+        private int myVar;
+
+        private int myVar2;
+        public int MyProperty2
+        {
+            get { return myVar2; }
+            set { myVar2 = value; }
+        }
+
+        public Payment()
+        {
+            Vencimento = DateTime.Now;
+            MyProperty = 2;
+            MyProperty2 = 8;
+        }
+
+        void Pay()
+        { }
+    }
+}
