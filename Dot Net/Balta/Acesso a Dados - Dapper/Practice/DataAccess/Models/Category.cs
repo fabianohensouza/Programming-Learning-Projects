75883580b26665586e2dc580f401944acef3d0f0
Fabiano Souza <fabianohensouza@gmail.com>
2023-02-07 21:53:39 -0300

    Stuying code

---
+++ Dot Net/Balta/Acesso a Dados - Dapper/Practice/DataAccess/Models/Category.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DataAccess.Models
+{
+    class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
--- Dot Net/Balta/Acesso a Dados - Dapper/Practice/DataAccess/Program.cs
+++ Dot Net/Balta/Acesso a Dados - Dapper/Practice/DataAccess/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dapper;
+using DataAccess.Models;
 using Microsoft.Data.SqlClient;
 
 namespace DataAccess // Note: actual namespace depends on the project name.
@@ -8,7 +10,38 @@ namespace DataAccess // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
-            Console.WriteLine("Hello World!");
+
+            //using block to dispose this block of code after ran it
+
+            //With ADO.NET
+            /*using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Connected");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }*/
+
+            //With Dapper
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
         }
     }
 }
