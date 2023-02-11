eba766696173b7e8e80db93688eaf4441e329e2d
Fabiano Souza <fabianohensouza@gmail.com>
2023-02-11 09:21:10 -0300

    Stuying code

--- Dot Net/Balta/Acesso a Dados - Dapper/Practice/DataAccess/Program.cs
+++ Dot Net/Balta/Acesso a Dados - Dapper/Practice/DataAccess/Program.cs
@@ -38,7 +38,8 @@ namespace DataAccess // Note: actual namespace depends on the project name.
 
             using (var connection = new SqlConnection(connectionString))
             {
-                UpdateCategory(connection);
+                //UpdateCategory(connection);
+                CreateManyCategory(connection);
                 ListCategories(connection);
                 //CreateCategory(connection);
             }
@@ -99,6 +100,61 @@ namespace DataAccess // Note: actual namespace depends on the project name.
                 title = "Backend 2023"
             });
             Console.WriteLine($"{rows} registros atualizados.");
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon-aws";
+            category.Summary = "Amazon Web Service";
+            category.Order = 9;
+            category.Description = "Treinamento Amazon Aws";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria Nova";
+            category2.Url = "categoria-nova";
+            category2.Summary = "Nova categoria";
+            category2.Order = 10;
+            category2.Description = "Nova categoria de treinamento";
+            category2.Featured = true;
+
+            var insertSql = $@"INSERT  
+                                [Category] 
+                            VALUES (
+                                @Id, 
+                                @Title, 
+                                @Url, 
+                                @Summary, 
+                                @Order,
+                                @Description, 
+                                @Featured)";
+
+            //Execute is used to Insert, Update and Delet operations
+            var rows = connection.Execute(insertSql, new[]{
+                new{
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                },
+                new{
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                },
+            });
+            Console.WriteLine($"{rows} linhas inseridas.");
         }
     }
 }
