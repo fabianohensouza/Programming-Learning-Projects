97474e1f752d7717416a8a238f451311c86ba8ed
Fabiano Souza <fabianohensouza@gmail.com>
2023-04-01 11:24:19 -0300

    Stuying code

---
+++ Dot Net/Balta/Fundamentos do Entity Framework/2808/Practice/Blog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Blog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var ctx = new Data.BlogDataContext();
+        }
+    }
+}
