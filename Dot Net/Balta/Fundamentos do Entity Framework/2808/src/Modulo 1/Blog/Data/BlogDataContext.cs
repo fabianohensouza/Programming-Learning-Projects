876f7832a69f5a279925a0e7207870504a962996
Fabiano Souza <fabianohensouza@gmail.com>
2023-04-25 21:34:16 -0300

    Stuying code

--- Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 1/Blog/Data/BlogDataContext.cs
+++ Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 1/Blog/Data/BlogDataContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,9 @@ namespace Blog.Data
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=Blog;Encrypt=false;User ID=sa;Password=1q2w3e4r@#$");
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=Blog;Encrypt=false;User ID=sa;Password=1q2w3e4r@#$");
+            //options.LogTo(Console.WriteLine);
+        }
     }
 }
--- Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 1/Blog/Program.cs
+++ Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 1/Blog/Program.cs
@@ -12,35 +12,61 @@ namespace Blog
         {
             using var context = new BlogDataContext();
 
-            var user = new User
-            {
-                Name = "Fabiano Souza",
-                Slug = "fabiano-souza",
-                Email = "fabiano.souza@gmail.com",
-                Bio = ".NET Student",
-                Image = "https://image.jpg",
-                PasswordHash = "12345678"
-            };
+            // var user = new User
+            // {
+            //     Name = "Fabiano Souza",
+            //     Slug = "fabiano-souza",
+            //     Email = "fabiano.souza@gmail.com",
+            //     Bio = ".NET Student",
+            //     Image = "https://image.jpg",
+            //     PasswordHash = "12345678"
+            // };
 
-            var category = new Category
-            {
-                Name = "BackEnd",
-                Slug = "backend"
-            };
+            // var category = new Category
+            // {
+            //     Name = "BackEnd",
+            //     Slug = "backend"
+            // };
 
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<p>Hello World!</p>",
-                Slug = "starting-with-ef-core",
-                Summary = "In this article we will laern about EF Core",
-                Title = "Starting With EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now
-            };
+            // var post = new Post
+            // {
+            //     Author = user,
+            //     Category = category,
+            //     Body = "<p>Hello World!</p>",
+            //     Slug = "starting-with-ef-core",
+            //     Summary = "In this article we will laern about EF Core",
+            //     Title = "Starting With EF Core",
+            //     CreateDate = DateTime.Now,
+            //     LastUpdateDate = DateTime.Now
+            // };
 
-            context.Posts.Add(post);
+            // context.Posts.Add(post);
+            // context.SaveChanges();
+
+            // var posts = context
+            //     .Posts
+            //     .AsNoTracking()
+            //     .Include(x => x.Author)
+            //     .Include(x => x.Category)
+            //     .Where(x => x.AuthorId == 4)
+            //     .OrderBy(x => x.LastUpdateDate)
+            //     .ToList();
+
+            // foreach (var post in posts)
+            //     Console.WriteLine($"{post.Title} - Escrito por: {post.Author?.Name} - Categoria {post.Category?.Name}");
+
+
+
+            var post = context
+                .Posts
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderBy(x => x.LastUpdateDate)
+                .FirstOrDefault();
+
+            post.Author.Name = "Fabiano Souza";
+
+            context.Posts.Update(post);
             context.SaveChanges();
 
         }
