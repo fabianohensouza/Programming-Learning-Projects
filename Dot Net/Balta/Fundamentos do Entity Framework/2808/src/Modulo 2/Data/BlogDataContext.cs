1b1267f12cb9ff1221636dd8ff36f8890f3bacac
Fabiano Souza <fabianohensouza@gmail.com>
2023-04-26 21:32:11 -0300

    Stuying code

--- Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 2/Data/BlogDataContext.cs
+++ Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 2/Data/BlogDataContext.cs
@@ -14,6 +14,6 @@ namespace Blog.Data
         // public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false");
     }
 }
--- Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 2/Models/Post.cs
+++ Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 2/Models/Post.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,8 +8,6 @@ namespace Blog.Models
     [Table("Post")]
     public class Post
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public string Title { get; set; }
@@ -18,10 +17,9 @@ namespace Blog.Models
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
 
-        [ForeignKey("CategoryId")] public int CategoryId { get; set; }
         public Category Category { get; set; }
-
-        [ForeignKey("AuthorId")] public int AuthorId { get; set; }
         public User Author { get; set; }
+
+        public IList<Tag> Tags { get; set; }
     }
 }
