12426c616d0ab2809f3213282aba379c3145aead
Fabiano Souza <fabianohensouza@gmail.com>
2023-04-27 21:25:42 -0300

    Stuying code

---
+++ Dot Net/Balta/Fundamentos do Entity Framework/2808/src/Modulo 2/Data/Mappings/PostMap.cs
@@ -0,0 +1,37 @@
+using System;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            //Table
+            builder.ToTable("Post");
+
+            //primary Key
+            builder.HasKey(x => x.Id);
+
+            //Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            //Properties
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+            //.HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+
+            // Indexes
+            builder.HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+        }
+    }
+}
