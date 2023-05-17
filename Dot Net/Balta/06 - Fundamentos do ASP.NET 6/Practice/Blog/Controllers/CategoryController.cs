9ef2bfa1993a773a05fad8f1a9f2b60751c52622
Fabiano Souza <fabianohensouza@gmail.com>
2023-05-16 21:59:57 -0300

    Stuying code

--- Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Controllers/CategoryController.cs
+++ Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,72 @@ namespace Blog.Controllers
         {
             var categories = await context.Categories.ToListAsync();
             return Ok(categories);
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<ActionResult> GetByIdAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            var category = await context
+                                 .Categories
+                                 .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpGet("v1/categories")]
+        public async Task<ActionResult> PostAsync(
+            [FromBody] Category model,
+            [FromServices] BlogDataContext context)
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            return Created($"v1/categories/{model.Id}", model);
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<ActionResult> PutAsync(
+            [FromBody] Category model,
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            var category = await context
+                                 .Categories
+                                 .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return Ok(category);
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<ActionResult> DeleteAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            var category = await context
+                                 .Categories
+                                 .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return Ok(category);
         }
     }
 }
