40aa197e2ee0d9bd3ea48fde1733dbdf2cd7f439
Fabiano Souza <fabianohensouza@gmail.com>
2023-05-17 21:52:50 -0300

    Stuying code

--- Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Controllers/CategoryController.cs
+++ Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Data;
 using Blog.Models;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,8 +13,15 @@ namespace Blog.Controllers
         public async Task<ActionResult> GetAsync(
             [FromServices] BlogDataContext context)
         {
-            var categories = await context.Categories.ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await context.Categories.ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05XE08 - Falha interna");
+            }
         }
 
         [HttpGet("v1/categories/{id:int}")]
@@ -21,47 +29,85 @@ namespace Blog.Controllers
             [FromRoute] int id,
             [FromServices] BlogDataContext context)
         {
-            var category = await context
+            try
+            {
+                var category = await context
                                  .Categories
                                  .FirstOrDefaultAsync(x => x.Id == id);
 
-            if (category == null)
-                return NotFound();
+                if (category == null)
+                    return NotFound();
 
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05XE07 - Falha interna");
+            }
+            
         }
 
-        [HttpGet("v1/categories")]
+        [HttpPost("v1/categories")]
         public async Task<ActionResult> PostAsync(
-            [FromBody] Category model,
+            [FromBody] CreateCategoryViewModel model,
             [FromServices] BlogDataContext context)
         {
-            await context.Categories.AddAsync(model);
-            await context.SaveChangesAsync();
+            try
+            {
+                var category = new Category 
+                {
+                    Id = 0,
+                    Posts = null,
+                    Name = model.Name,
+                    Slug = model.Slug.ToLower(),
+                };
 
-            return Created($"v1/categories/{model.Id}", model);
+                await context.Categories.AddAsync(category);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{category.Id}", category);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05XE09 - Não foi possível incluir a categoria");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05XE10 - Falha interna");
+            }
         }
 
         [HttpPut("v1/categories/{id:int}")]
         public async Task<ActionResult> PutAsync(
-            [FromBody] Category model,
+            [FromBody] CreateCategoryViewModel model,
             [FromRoute] int id,
             [FromServices] BlogDataContext context)
-        {
-            var category = await context
+        {          
+            try
+            {
+                var category = await context
                                  .Categories
                                  .FirstOrDefaultAsync(x => x.Id == id);
 
-            if (category == null)
-                return NotFound();
+                if (category == null)
+                    return NotFound();
 
-            category.Name = model.Name;
-            category.Slug = model.Slug;
+                category.Name = model.Name;
+                category.Slug = model.Slug;
 
-            context.Categories.Update(category);
-            await context.SaveChangesAsync();
+                context.Categories.Update(category);
+                await context.SaveChangesAsync();
 
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05XE11 - Não foi possível alterar a categoria");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05XE12 - Falha interna");
+            }
         }
 
         [HttpDelete("v1/categories/{id:int}")]
@@ -69,17 +115,28 @@ namespace Blog.Controllers
             [FromRoute] int id,
             [FromServices] BlogDataContext context)
         {
-            var category = await context
-                                 .Categories
-                                 .FirstOrDefaultAsync(x => x.Id == id);
+            try
+            {
+                var category = await context
+                                     .Categories
+                                     .FirstOrDefaultAsync(x => x.Id == id);
 
-            if (category == null)
-                return NotFound();
+                if (category == null)
+                    return NotFound();
 
-            context.Categories.Remove(category);
-            await context.SaveChangesAsync();
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
 
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05XE13 - Não foi possível excluir a categoria");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05XE14 - Falha interna");
+            }
         }
     }
 }
