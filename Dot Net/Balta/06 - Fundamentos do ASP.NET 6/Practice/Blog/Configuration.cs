52767ca3330520f7678da73bff058424863c44c0
Fabiano Souza <fabianohensouza@gmail.com>
2023-05-22 21:54:36 -0300

    Stuying code

---
+++ Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog
+{
+    public static class Configurationcs
+    {
+        public static string JwtKey { get; set; } = "JhBva=-aDlTrFPZsexvHtaYBpUzhKDS8"; 
+    }
+}
---
+++ Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Services/TokenServices.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Models;
+
+namespace Blog.Services
+{
+    public class TokenServices
+    {
+        public string GenerateToken(User user)
+        {
+
+
+        }
+    }
+}
