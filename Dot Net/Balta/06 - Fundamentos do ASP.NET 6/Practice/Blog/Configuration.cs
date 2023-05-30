39dc4b870a9d485388ca0db9fb32648a868a3e21
Fabiano Souza <fabianohensouza@gmail.com>
2023-05-29 21:49:23 -0300

    Stuying code

--- Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Configuration.cs
+++ Dot Net/Balta/06 - Fundamentos do ASP.NET 6/Practice/Blog/Configuration.cs
@@ -5,5 +5,14 @@
         public static string JwtKey = "JhBva=-aDlTrFPZsexvHtaYBpUzhKDS8";
         public static string ApiKeyName = "api_key";
         public static string ApiKey = "Curso=aPi-TrFPZsexvHtaYBpUzhKDS8";
+        public static SmtpConfiguration Smtp = new();
+
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
