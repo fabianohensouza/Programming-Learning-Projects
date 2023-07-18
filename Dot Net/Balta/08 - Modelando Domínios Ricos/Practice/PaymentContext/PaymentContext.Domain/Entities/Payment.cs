cd60f17e8bc8d8b4e19c5256507ff00373e51f80
Fabiano Souza <fabianohensouza@gmail.com>
2023-07-18 07:12:18 -0300

    Defining rich domains

---
+++ Dot Net/Balta/08 - Modelando Domínios Ricos/Practice/PaymentContext/PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,34 @@
+
+namespace PaymentContext.Domain.Entities
+{
+    public abstract class Payment
+    {
+        public string Number { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Document { get; set; }
+        public string Owner { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class BoletoPayment : Payment
+    {
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; }
+    }
+
+    public class CreditCardPayment : Payment
+    {
+        public string CardHolderNamer { get; set; }
+        public string CardNumber { get; set; } //Last 4 credit card digits
+        public string LastTransactionNumber { get; set; }
+    }
+
+    public class PayPalPayment : Payment
+    {
+        public string TransactionCode { get; set; }
+    }
+}
---
+++ Dot Net/Balta/08 - Modelando Domínios Ricos/Practice/PaymentContext/PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,13 @@
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+    }
+}
