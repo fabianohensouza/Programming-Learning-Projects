de138d12b8c5270bc40ec835fc5d2661c1f1f100
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2023-10-20 13:41:42 -0300

    Initializing the TODO App

--- Dot Net/Balta/12 - Injeção de Dependencia/Demos/04 - final/Program.cs
+++ Dot Net/Balta/12 - Injeção de Dependencia/Demos/04 - final/Program.cs
@@ -2,7 +2,7 @@ using DependencyInjectionLifetimeSample.Services;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<IService, PrimaryService>();
+// builder.Services.AddTransient<IService, PrimaryService>();
 // builder.Services.AddTransient<IService, PrimaryService>();
 // builder.Services.AddTransient<IService, PrimaryService>();
 // builder.Services.AddTransient<IService, SecondaryService>();
@@ -19,14 +19,14 @@ builder.Services.AddTransient<IService, PrimaryService>();
 // builder.Services.TryAddTransient<IService, PrimaryService>();
 // builder.Services.TryAddTransient<IService, PrimaryService>();
 
-// builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>());
-// builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>()); // Permite
+builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>());
+builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>()); // Permite
 // builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, SecondaryService>()); // Não permite
 
 var app = builder.Build();
 
 // app.MapGet("/", (IService primaryService) => Results.Ok(primaryService.GetType().Name));
-app.MapGet("/", (IEnumerable<IService> services) => Results.Ok(services.Select(x=>x.GetType().Name)));
+app.MapGet("/", (IEnumerable<IService> services) => Results.Ok(services.Select(x => x.GetType().Name)));
 
 app.Run();
 
--- Dot Net/Balta/12 - Injeção de Dependencia/Demos/05 - final/Program.cs
+++ Dot Net/Balta/12 - Injeção de Dependencia/Demos/05 - final/Program.cs
@@ -14,6 +14,7 @@ using (var scope = app.Services.CreateScope())
     var service = scope
         .ServiceProvider
         .GetRequiredService<IWeatherService>();
+
     service.Get();
 }
 
